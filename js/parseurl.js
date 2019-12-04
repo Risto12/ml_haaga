6c3bd2718e584d8beb57ce3405b71e68e6c7c04b
Risto <risto.vesanto1112@gmail.com>
2019-12-04 18:57:33 +0200

    beta ready

--- js/parseurl.js
+++ js/parseurl.js
@@ -1,7 +1,13 @@
-function parseurl(){
+(function parseurl(){
     const params = new URLSearchParams(location.search);
-    document.getElementById("inputEmail4").value = params.get("email")
-    
-}
+    document.getElementById("name").value = params.get("name")
+    document.getElementById("email").value = params.get("email")
+    document.getElementById("birthYear").value = params.get("birthYear")
+    document.getElementById("phoneNumber").value = params.get("phoneNumber")
+    document.getElementById("postalCode").value = params.get("postalCode")
+    document.getElementById("luggage").value = params.get("luggage")
+    document.getElementById("duration").checked = params.get("duration") === "true" ? true : false
+    document.getElementById("kela").checked = params.get("kela") === "true" ? true : false
+})();
 
-parseurl()
+//?email=world&name=risto&phoneNumber=02100101&postalCode=3131&luggage=311&birthYear=1999&duration=true&kela=true
